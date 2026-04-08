ef147dadee68d83744251aefe43b1831eacfc5b3
xC3FFF0E <support@hydrv.app>
2026-04-08 10:23:18 +0800

    Apply launcher icon on startup

--- HYDRV/app/src/main/java/app/hydra/manager/MainActivity.kt
+++ HYDRV/app/src/main/java/app/hydra/manager/MainActivity.kt
@@ -126,6 +126,7 @@ class MainActivity : AppCompatActivity() {
             R.color.subtext
         )
         supportActionBar?.hide()
+        AppIconPreferences.applySavedIcon(this)
         val homeTab = findViewById<View>(R.id.nav_home_tab)
         val downloadTab = findViewById<View>(R.id.nav_download_tab)
         val settingsTab = findViewById<View>(R.id.nav_settings_tab)
@@ -424,5 +425,6 @@ class MainActivity : AppCompatActivity() {
         return packageName
     }
 }
+
 
 
