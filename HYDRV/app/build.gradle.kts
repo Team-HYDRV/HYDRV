c08cbc0496aa38a34be3979e915aef59ee7c412e
xC3FFF0E <support@hydrv.app>
2026-04-08 17:07:40 +0800

    Bump version to 1.0.7

--- HYDRV/app/build.gradle.kts
+++ HYDRV/app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "app.hydra.manager"
         minSdk = 23
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
