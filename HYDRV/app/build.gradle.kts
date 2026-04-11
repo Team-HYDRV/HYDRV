f8a1a906010caca68e2bb0f7e3c48bbcb95589e8
xC3FFF0E <support@hydrv.app>
2026-04-11 15:58:13 +0800

    Release 1.0.8

--- HYDRV/app/build.gradle.kts
+++ HYDRV/app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "app.hydra.manager"
         minSdk = 23
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
